6bb224c1395c6b9b757085b0af2d2197a1ae3c71
mz01-omega <omega@mz.co.kr>
2023-07-07 20:25:58 +0900

    Building department findAll.....

--- src/main/java/kr/co/mz/tutorial/jsp/dao/DepartmentDao.java
+++ src/main/java/kr/co/mz/tutorial/jsp/dao/DepartmentDao.java
@@ -1,13 +1,33 @@
 package kr.co.mz.tutorial.jsp.dao;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import javax.sql.DataSource;
+import kr.co.mz.tutorial.jsp.db.HikariPoolFactory;
+import kr.co.mz.tutorial.jsp.db.QueryManager;
 import kr.co.mz.tutorial.jsp.dto.DepartmentDto;
 
 public class DepartmentDao {
 
+  public DataSource dataSource;
+
+  public DepartmentDao() {
+    dataSource = new HikariPoolFactory().createHikariDataSource();
+  }
+
   public List<DepartmentDto> findAll() throws SQLException {
+    try (var conn = dataSource.getConnection()) {
+      var pst = conn.prepareStatement(QueryManager.getQuery("SELECT_ALL_DEPARTMENT"));
+      var rs = pst.executeQuery();
+      var departmentList = new ArrayList<DepartmentDto>();
+      while (rs.next()) {
+        var departmentDto = new DepartmentDto().fromResultSet(rs);
+        departmentList.add(departmentDto);
+      }
+
+    }
     return null;
   }
 
@@ -15,5 +35,11 @@ public class DepartmentDao {
     return Optional.empty();
   }
 
-  
+  public Optional<DepartmentDto> findOneByDepartmentName(String departmentName) throws SQLException {
+    return Optional.empty();
+  }
+
+  public Optional<Integer> insertOne(DepartmentDto departmentDto) throws SQLException {
+    return Optional.empty();
+  }
 }
